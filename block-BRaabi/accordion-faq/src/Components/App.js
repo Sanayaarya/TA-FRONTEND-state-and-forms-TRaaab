a0f99e927f8781ef641fd8eca122a8d4213fceb1
Sanaya Arya <sanayaarya0123@gmail.com>
2022-11-17 16:03:12 +0530

    Assignment: Accordion FAQ

---
+++ block-BRaabi/accordion-faq/src/Components/App.js
@@ -0,0 +1,42 @@
+
+
+import React from 'react';
+import data from '../data';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeIndex: null,
+    };
+  }
+  render() {
+    return (
+      <div className="container">
+        <h1 className="center">FAQs</h1>
+        <ul className="question-list">
+          {data.map((faq, index) => (
+            <li>
+              <h2
+                className={index === this.state.activeIndex ? 'question' : ''}
+                onClick={() => {
+                  this.setState({
+                    activeIndex:
+                      this.state.activeIndex === index ? null : index,
+                  });
+                }}
+              >
+                {faq.Q} {this.state.activeIndex === index ? '☝🏻' : '👇🏻'}
+              </h2>
+              {index === this.state.activeIndex && (
+                <p className="answer">{faq.A}</p>
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
