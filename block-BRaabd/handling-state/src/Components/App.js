9925f9482a81a8a665494dd76f7db512a6dfd3e6
Sanaya Arya <sanayaarya0123@gmail.com>
2022-11-18 11:28:18 +0530

    Assignment: Handling State

---
+++ block-BRaabd/handling-state/src/Components/App.js
@@ -0,0 +1,40 @@
+
+
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: 'basketball',
+    };
+  }
+
+  render() {
+    let labels = ['basketball', 'pubg', 'tiger', 'phone', 'laptop', 'cricket'];
+    return (
+      <div className="container center">
+        <h1>Show Images</h1>
+        <div className="center">
+          {labels.map((label) => (
+            <button
+              onClick={() => {
+                this.setState({ active: label });
+              }}
+            >
+              {label}
+            </button>
+          ))}
+        </div>
+        <div className="img-box">
+          <img
+            src={`./images/${this.state.active}.jpg`}
+            alt={this.state.active}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
